c4822e7c51bf9b7a002bd92806c137b91589c33b
Mburu Njoroge <90961555+LONGMANKE@users.noreply.github.com>
2022-10-26 11:15:23 +0100

    Add files via upload

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * updates the value it points to 98
+ * @n: pointer to int n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
